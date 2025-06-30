7661f96ecde01dc573a465fedc8f1f1ede47632d
Guiziin227 <gh63388@gmail.com>
2025-06-29 21:42:06 -0300

    AutorRep

---
+++ src/main/java/com/github/guiziin227/livraria/repositories/AutorRepository.java
@@ -0,0 +1,8 @@
+package com.github.guiziin227.livraria.repositories;
+
+
+import com.github.guiziin227.livraria.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+}
